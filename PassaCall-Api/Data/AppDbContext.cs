792d8b0474724b5e83d5075575c7ecceb2fb684e
Desktop <Desktop@DESKTOP-BHH85IN>
2024-09-17 07:29:32 -0300

    Adicionar arquivos de projeto.

---
+++ PassaCall-Api/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassaCall_Api.Models;
+
+namespace PassaCall_Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TeamModel> Team { get; set; }
+
+    }
+}
---
+++ PassaCall-Api/Models/TeamModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PassaCall_Api.Models
+{
+    public class TeamModel
+    {
+        //direitinha [JsonIgnore] - não aparece no swagger
+        public int IdTeam { get; set; }
+        public string NameTeam { get; set; }
+
+    }
+}
